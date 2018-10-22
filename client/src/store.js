85bac5ffb6ec8691813d21576a0a5a7c331e6e3a
Kyle Plaugher <kaplaugher94@gmail.com>
2018-10-22 18:43:19 -0400

    set up jwt

--- client/src/store.js
+++ client/src/store.js
@@ -3,8 +3,7 @@ import Vuex from 'vuex';
 
 import { defaultClient as apolloClient } from './main';
 
-import { GET_POSTS } from './queries';
-import { gql } from 'apollo-boost';
+import { GET_POSTS, SIGNIN_USER } from './queries';
 
 Vue.use(Vuex);
 
@@ -38,6 +37,19 @@ export default new Vuex.Store({
         .catch(err => {
           commit('setLoading', false);
           console.error(err);
+        });
+    },
+    signinUser: ({ commit }, payload) => {
+      apolloClient
+        .mutate({
+          mutation: SIGNIN_USER,
+          variables: payload
+        })
+        .then(({ data }) => {
+          console.log(data.signinUser);
+        })
+        .catch(err => {
+          console.err(err);
         });
     }
   },
