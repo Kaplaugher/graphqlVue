5b5f87daed69ae06e00b0db5ecb8eeb4761ca81d
Kaplaugher <kaplaugher94@gmail.com>
2018-11-11 20:48:59 -0500

    infiniteScroll query not working

--- client/src/views/Posts.vue
+++ client/src/views/Posts.vue
@@ -1,13 +1,62 @@
 <template>
-    <v-container>
-        <h1>Posts</h1>
+    <v-container text-xs-center v-if="infiniteScrollPosts">
+        <div v-for="post in infiniteScrollPosts.posts" :key="post._id">
+            <img :src="post.imageUrl" height="100px">
+            <h3>{{post.title}}</h3>
+        </div>
+        <v-btn @click="showMorePosts" v-if="showMoreEnabled">Fetch More</v-btn>
 
     </v-container>
 </template>
 
 <script>
+import { INFINITE_SCROLL_POSTS } from '../queries.js';
+
+const pageSize = 2;
+
 export default {
-  name: 'Posts'
+  name: 'Posts',
+  data() {
+    return {
+      pageNum: 1,
+      showMoreEnabled: true
+    };
+  },
+  apollo: {
+    infiniteScrollPosts: {
+      query: INFINITE_SCROLL_POSTS,
+      variable: {
+        pageNum: 1,
+        pageSize
+      }
+    }
+  },
+  methods: {
+    showMore() {
+      //   fetch more data and transform original result
+      this.pageNum += 1;
+      this.$apollo.queries.infiniteScrollPosts.fetchMore({
+        variables: {
+          // pagenum incremented by one
+          pageNum: this.pageNum,
+          pageSize: this.pageSize
+        },
+        updateQuery: (prevResult, { fetchMoreResult }) => {
+          const newPosts = fetchMoreResult.infiniteScrollPosts.posts;
+          const hasMore = fetchMoreResult.infiniteScrollPosts.hasMore;
+          this.showMoreEnabled = hasMore;
+          return {
+            infiniteScrollPosts: {
+              __typename: prevResult.infiniteScrollPosts.__typename,
+              // merge pre posts with new posts
+              posts: [...prevResult.infiniteScrollPosts.posts, ...newPosts],
+              hasMore
+            }
+          };
+        }
+      });
+    }
+  }
 };
 </script>
 
